e396cde35d8327a0e0ac9cdd741ae60b1c893877
bentrask10 <bt2175@stern.nyu.edu>
2025-03-04 20:38:20 -0500

    Update LoadWise2_0App.swift

--- LoadWise2.0/LoadWise2_0App.swift
+++ LoadWise2.0/LoadWise2_0App.swift
@@ -11,7 +11,11 @@ import SwiftUI
 struct LoadWiseApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if UserDefaults.standard.bool(forKey: "hasCompletedOnboarding") {
+                ContentView()
+            } else {
+                OnboardingView()
+            }
         }
     }
 }
