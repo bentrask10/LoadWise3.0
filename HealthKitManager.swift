f01f5c4a2f1ca6470a10775d7c8950694348cf7f
bentrask10 <bt2175@stern.nyu.edu>
2025-03-04 20:42:15 -0500

    HealthKitManager.swift

--- HealthKitManager.swift
+++ HealthKitManager.swift
@@ -12,45 +12,103 @@ class HealthKitManager {
     static let shared = HealthKitManager()
     let healthStore = HKHealthStore()
 
-    // Define the data types we want to read
     private let heartRateType = HKQuantityType.quantityType(forIdentifier: .heartRate)!
     private let distanceType = HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!
-
-    // Request permission to access HealthKit data
+    private let activeEnergyType = HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned)!
+    private let restingEnergyType = HKQuantityType.quantityType(forIdentifier: .basalEnergyBurned)!
+    private let stepsType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+    private let runningPowerType = HKQuantityType.quantityType(forIdentifier: .runningPower)!
+    private let runningSpeedType = HKQuantityType.quantityType(forIdentifier: .runningSpeed)!
+    private let strideLengthType = HKQuantityType.quantityType(forIdentifier: .runningStrideLength)!
+    private let verticalOscillationType = HKQuantityType.quantityType(forIdentifier: .runningVerticalOscillation)!
+    private let groundContactTimeType = HKQuantityType.quantityType(forIdentifier: .runningGroundContactTime)!
+    
+    
     func requestHealthKitPermission(completion: @escaping (Bool, Error?) -> Void) {
-        let readTypes: Set<HKObjectType> = [heartRateType, distanceType]
+        let readTypes: Set<HKObjectType> = [
+            heartRateType, distanceType, activeEnergyType, restingEnergyType, stepsType,
+            runningPowerType, runningSpeedType, strideLengthType, verticalOscillationType,
+            groundContactTimeType
+        ]
 
-        healthStore.requestAuthorization(toShare: [], read: readTypes) { success, error in
+        healthStore.requestAuthorization(toShare: nil, read: readTypes) { success, error in
             completion(success, error)
         }
     }
-
-    // Fetch the latest heart rate measurement
-    func fetchLatestHeartRate(completion: @escaping (Double?) -> Void) {
+    
+    private func fetchMostRecentSample(for type: HKQuantityType, unit: HKUnit, completion: @escaping (Double?) -> Void) {
         let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
-        let query = HKSampleQuery(sampleType: heartRateType, predicate: nil, limit: 1, sortDescriptors: [sortDescriptor]) { _, results, error in
+        let query = HKSampleQuery(sampleType: type, predicate: nil, limit: 1, sortDescriptors: [sortDescriptor]) { _, results, error in
             guard let sample = results?.first as? HKQuantitySample, error == nil else {
                 completion(nil)
                 return
             }
-            let heartRate = sample.quantity.doubleValue(for: HKUnit(from: "count/min"))
-            completion(heartRate)
+            completion(sample.quantity.doubleValue(for: unit))
         }
         healthStore.execute(query)
     }
-
-    // Fetch total distance from today's workouts
-    func fetchTotalDistance(completion: @escaping (Double?) -> Void) {
-        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
-        let query = HKSampleQuery(sampleType: distanceType, predicate: nil, limit: 1, sortDescriptors: [sortDescriptor]) { _, results, error in
-            guard let sample = results?.first as? HKQuantitySample, error == nil else {
+    
+    func fetchAvgHeartRate(forWorkout workout: HKWorkout, completion: @escaping (Double?) -> Void) {
+        let predicate = HKQuery.predicateForObjects(from: workout)
+        let query = HKStatisticsQuery(quantityType: heartRateType, quantitySamplePredicate: predicate, options: .discreteAverage) { _, result, _ in
+            guard let average = result?.averageQuantity() else {
                 completion(nil)
                 return
             }
-            let distance = sample.quantity.doubleValue(for: HKUnit.mile())
-            completion(distance)
+            completion(average.doubleValue(for: HKUnit.count().unitDivided(by: HKUnit.minute())))
         }
         healthStore.execute(query)
     }
+    
+    func fetchTotalWalkingRunningDistance(completion: @escaping (Double?) -> Void) {
+        fetchMostRecentSample(for: distanceType, unit: HKUnit.meter(), completion: completion)
+    }
+    
+    func fetchTotalActiveEnergy(completion: @escaping (Double?) -> Void) {
+        fetchMostRecentSample(for: activeEnergyType, unit: HKUnit.kilocalorie(), completion: completion)
+    }
+    
+    func fetchTotalRestingEnergy(completion: @escaping (Double?) -> Void) {
+        fetchMostRecentSample(for: restingEnergyType, unit: HKUnit.kilocalorie(), completion: completion)
+    }
+    
+    func fetchTotalSteps(completion: @escaping (Double?) -> Void) {
+        fetchMostRecentSample(for: stepsType, unit: HKUnit.count(), completion: completion)
+    }
+    
+    func fetchAvgRunningPower(completion: @escaping (Double?) -> Void) {
+        fetchMostRecentSample(for: runningPowerType, unit: HKUnit.watt(), completion: completion)
+    }
+    
+    func fetchAvgRunningSpeed(completion: @escaping (Double?) -> Void) {
+        fetchMostRecentSample(for: runningSpeedType, unit: HKUnit.meter().unitDivided(by: HKUnit.second()), completion: completion)
+    }
+    
+    func fetchAvgRunningStrideLength(completion: @escaping (Double?) -> Void) {
+        fetchMostRecentSample(for: strideLengthType, unit: HKUnit.meter(), completion: completion)
+    }
+    
+    func fetchAvgVerticalOscillation(completion: @escaping (Double?) -> Void) {
+        fetchMostRecentSample(for: verticalOscillationType, unit: HKUnit.meter(), completion: completion)
+    }
+    
+    func fetchAvgGroundContactTime(completion: @escaping (Double?) -> Void) {
+        fetchMostRecentSample(for: groundContactTimeType, unit: HKUnit.second(), completion: completion)
+    }
+    
+    func fetchLatestWorkout(completion: @escaping (HKWorkout?) -> Void) {
+        let workoutType = HKObjectType.workoutType()
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
+        
+        let query = HKSampleQuery(sampleType: workoutType, predicate: nil, limit: 1, sortDescriptors: [sortDescriptor]) { _, results, error in
+            guard let workout = results?.first as? HKWorkout, error == nil else {
+                completion(nil)
+                return
+            }
+            completion(workout)
+        }
+        
+        healthStore.execute(query)
+    }
+
 }
-
